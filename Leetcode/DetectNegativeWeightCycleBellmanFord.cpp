c4ef5e5a0090cce915886481ecc3953d87d8dbfd
Roshan-T <82012823+Roshan-T@users.noreply.github.com>
2024-07-13 12:15:50 +0545

    DetectNegativeWeightCycleBellmanFord program file

---
+++ Leetcode/DetectNegativeWeightCycleBellmanFord.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node {
+    int u, v, wt;
+    node(int first, int second, int weight) {
+        u = first;
+        v = second;
+        wt = weight;
+    }
+};
+
+int main() {
+    int N = 6, m = 7;
+    vector<node> edges;
+    edges.push_back(node(0, 1, 5));
+    edges.push_back(node(1, 2, -2));
+    edges.push_back(node(1, 5, -3));
+    edges.push_back(node(2, 4, 3));
+    edges.push_back(node(3, 2, 6));
+    edges.push_back(node(3, 4, -2));
+    edges.push_back(node(5, 3, 1));
+
+    int src = 0;
+    int inf = 10000000;
+    vector<int> dist(N, inf);
+    dist[src] = 0;
+
+    // Relax all edges N-1 times
+    for(int i = 1; i <= N-1; i++) {
+        for(auto it: edges) {
+            if(dist[it.u] + it.wt < dist[it.v]) {
+                dist[it.v] = dist[it.u] + it.wt;
+            }
+        }
+        // Print distances after each iteration
+        for(int i = 0; i < N; i++) {
+            cout << dist[i] << " ";
+        }
+        cout << endl;
+    }
+
+    // Check for negative weight cycle
+    int fl = 0;
+    for(auto it: edges) {
+        if(dist[it.u] + it.wt < dist[it.v]) {
+            cout << -1;
+            fl = 1;
+            break;
+        }
+    }
+
+    if(!fl) {
+        for(int i = 0; i < N; i++) {
+            cout << dist[i] << " ";
+        }
+    }
+
+    return 0;
+}
